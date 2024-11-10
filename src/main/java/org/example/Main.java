f447517d88db3ac1c3357aeecf3d34e1569480d2
Ando Keiko <gajumaro.no.ki@gmail.com>
2024-11-11 00:09:26 +0900

    無限ループ

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,23 +4,30 @@ import java.util.Scanner;
 
 public class Main {
 
-  public static void main(String[] args) {
+ public static void main(String[] args) {
 
-   Scanner scanner = new Scanner(System.in);
-   Calculator calculator = new Calculator();
-   printer printer = new printer();
+  Scanner scanner = new Scanner(System.in);
+  Calculator calculator = new Calculator();
+  printer printer = new printer();
 
-   System.out.print("1番目の数字を入力してください:　");
-   int num1 = scanner.nextInt();
+  while (true) {
+   try {
+    System.out.print("1番目の数字を入力してください:　");
+    int num1 = scanner.nextInt();
 
-   System.out.print("演算子を入力してください (+, -, *, /):　");
-   char operator = scanner.next().charAt(0);
+    System.out.print("演算子を入力してください (+, -, *, /):　");
+    char operator = scanner.next().charAt(0);
 
-   System.out.print("2番目の数字を入力してください:　");
-   int num2 = scanner.nextInt();
+    System.out.print("2番目の数字を入力してください:　");
+    int num2 = scanner.nextInt();
 
-   int result = calculator.calulate(num1, String.valueOf(operator), num2);
+    int result = calculator.calulate(num1, String.valueOf(operator), num2);
 
-   printer.printCalculator(result);
+    printer.printCalculator(result);
+
+   } catch (Exception e) {
+    scanner.nextLine();
+   }
   }
+ }
 }
