a19428f665cdcbf2b315ba9e0eed78ce86560bb5
Ando Keiko <gajumaro.no.ki@gmail.com>
2024-11-10 12:55:24 +0900

    微修正

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -10,13 +10,13 @@ public class Main {
    Calculator calculator = new Calculator();
    printer printer = new printer();
 
-   System.out.println("1番目の数字を入力してください:");
+   System.out.print("1番目の数字を入力してください:　");
    int num1 = scanner.nextInt();
 
-   System.out.println("演算子を入力してください (+, -, *, /):");
+   System.out.print("演算子を入力してください (+, -, *, /):　");
    char operator = scanner.next().charAt(0);
 
-   System.out.println("2番目の数字を入力してください:");
+   System.out.print("2番目の数字を入力してください:　");
    int num2 = scanner.nextInt();
 
    int result = calculator.calulate(num1, String.valueOf(operator), num2);
